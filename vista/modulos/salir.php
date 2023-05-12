8e4ad843b3beb7a737d5ffb7349ec6fb4b983e85
jhonatanSantiago13 <jhonale44@hotmail.com>
2023-05-12 13:15:07 -0600

    actualizar status token rememberme ok

--- vista/modulos/salir.php
+++ vista/modulos/salir.php
@@ -3,7 +3,18 @@
 /*Eliminar la cookie*/
 // setcookie("tkn","",time() - 60,"/tiendita2/");
 
+/*=============================================
+CAMBIAR EL ESTATUS DEL REMBERME
+=============================================*/
+
+if (isset($_COOKIE["tkn"])) {
+
+	$respuesta = ControladorUsuarios::ctrUpdateStatusSesion($_COOKIE["tkn"]);
+}
+
+
 session_destroy();
+
 
 echo "<script>
 
